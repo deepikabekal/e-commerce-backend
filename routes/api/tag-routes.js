34b7fb62b7f6be7e09ce96623e6d717ab2b35a99
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-28 02:20:20 +0400

    get routes for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,16 +1,49 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { findOne } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag
+  .findAll({
+    include : [Product]
+  })
+  .then(tagData => {
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag
+  .findOne({
+    include : [Product],
+    where : {
+      id : req.params.id
+    }
+  })
+  .then (tagData => {
+    if (!tagData)
+    {
+      res.status(404).json({message : 'No category found with this ID'});
+      return;
+    }
+
+    res.json(tagData);
+
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
